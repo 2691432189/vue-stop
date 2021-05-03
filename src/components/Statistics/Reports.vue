1f2fb8a12199f36087dd144e489335b2a1933e15
迷途橘 <2691432189@qq.com>
2021-05-03 14:42:29 +0800

    数据统计做完了

--- src/components/Statistics/Reports.vue
+++ src/components/Statistics/Reports.vue
@@ -1,12 +1,71 @@
 <template>
   <div>
-    reports
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">
+        首页
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item> 数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 面包屑导航 -->
+    <!-- 卡片 -->
+    <el-card class="box-card">
+      <div
+        id="main"
+        style="width: 600px;height:550px;"
+      />
+    </el-card>
   </div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
+import _ from 'lodash'
 export default {
-
+  data () {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  async mounted () {
+    var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    // 判断状态码是否正常
+    if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+    this.ReportsList = res.data
+    var option = _.merge(res.data, this.options)
+    myChart.setOption(option)
+  }
 }
 </script>
 
